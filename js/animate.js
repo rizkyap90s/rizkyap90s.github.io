6d8b01c8afa0a4ce76e7b4cdc04f83510db8499a
Kik <rizkie.kudo@gmail.com>
2021-08-10 22:38:59 +0800

    remove scroll

--- js/animate.js
+++ js/animate.js
@@ -11,8 +11,8 @@ const navside = document.querySelector(".navside");
 navside.classList.add("animate__animated", "animate__fadeInLeft");
 
 // SCROLL DOWN
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  introJunior.style.color = "blue";
-  introDeveloper.style.transform = `translate(0px, ${currentScrollPos * 2}%)`;
-};
+// window.onscroll = function () {
+//   let currentScrollPos = window.pageYOffset;
+//   introJunior.style.color = "blue";
+//   introDeveloper.style.transform = `translate(0px, ${currentScrollPos * 2}%)`;
+// };
