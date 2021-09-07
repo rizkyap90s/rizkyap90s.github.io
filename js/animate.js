16eec12c8b128313ef868939ff48128148705953
rizkyap90s <rizkie.kudo@gmail.com>
2021-09-07 11:55:16 +0800

    add editing content

--- js/animate.js
+++ js/animate.js
@@ -7,22 +7,26 @@ introJunior.style.setProperty("--animate-duration", "2s");
 
 const introDeveloper = document.querySelector(".intro .title .dev");
 introDeveloper.classList.add("animate__animated", "animate__fadeInRight");
+introDeveloper.style.setProperty("--animate-duration", "2s");
 
 const navside = document.querySelector(".navside");
 navside.classList.add("animate__animated", "animate__fadeInLeft");
 
 const progLanguage = document.querySelector(".ability-content .programming-language-content");
 const database = document.querySelector(".ability-content .database-content");
+const editing = document.querySelector(".ability-content .editing-content");
 
 // SCROLL DOWN
 window.addEventListener("scroll", function () {
-  //do stuff
   let currentScrollPos = window.pageYOffset;
-  if (currentScrollPos > 700) {
+  if (currentScrollPos > 600) {
     progLanguage.classList.add("animate__animated", "animate__fadeInBottomRight");
     database.classList.add("animate__animated", "animate__fadeInBottomLeft");
-    // progLanguage.style.setProperty("--animate-duration", "1.5s");
     progLanguage.style.opacity = "1";
     database.style.opacity = "1";
+  }
+  if (currentScrollPos > 900) {
+    editing.classList.add("animate__animated", "animate__fadeInUp");
+    editing.style.opacity = "1";
   }
 });
