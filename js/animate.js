b1c4b691ebb291856fe2d8c7ae9bfdf2bfe29f1d
rizkyap90s <rizkie.kudo@gmail.com>
2021-09-07 15:04:56 +0800

    add vector flower

--- js/animate.js
+++ js/animate.js
@@ -1,8 +1,12 @@
+const introBackground = document.querySelector(".intro");
+introBackground.classList.add("animate__animated", "animate__fadeIn");
+
 const introBrand = document.querySelector(".intro .brand");
 introBrand.classList.add("animate__animated", "animate__fadeInDown");
+introBrand.style.setProperty("--animate-duration", "2s");
 
 const introJunior = document.querySelector(".intro .title .jun");
-introJunior.classList.add("animate__animated", "animate__fadeInDown");
+introJunior.classList.add("animate__animated", "animate__fadeInLeftBig");
 introJunior.style.setProperty("--animate-duration", "2s");
 
 const introDeveloper = document.querySelector(".intro .title .dev");
@@ -11,6 +15,7 @@ introDeveloper.style.setProperty("--animate-duration", "2s");
 
 const navside = document.querySelector(".navside");
 navside.classList.add("animate__animated", "animate__fadeInLeft");
+navside.style.setProperty("--animate-duration", "2s");
 
 const progLanguage = document.querySelector(".ability-content .programming-language-content");
 const database = document.querySelector(".ability-content .database-content");
@@ -19,14 +24,14 @@ const editing = document.querySelector(".ability-content .editing-content");
 // SCROLL DOWN
 window.addEventListener("scroll", function () {
   let currentScrollPos = window.pageYOffset;
-  if (currentScrollPos > 600) {
+  if (currentScrollPos > 1100) {
     progLanguage.classList.add("animate__animated", "animate__fadeInBottomRight");
     database.classList.add("animate__animated", "animate__fadeInBottomLeft");
     progLanguage.style.opacity = "1";
     database.style.opacity = "1";
   }
-  if (currentScrollPos > 900) {
-    editing.classList.add("animate__animated", "animate__fadeInUp");
+  if (currentScrollPos > 1200) {
+    editing.classList.add("animate__animated", "animate__fadeInRightBig");
     editing.style.opacity = "1";
   }
 });
