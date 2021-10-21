7feb2d383cf6ead9da7f72cfad819894fc94472c
rizkyap90s <rizkie.kudo@gmail.com>
2021-10-21 14:34:49 +0800

    add content skill

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// const indev = document.querySelector(".indev");
+// window.addEventListener("click", function () {
+//   indev.style.display = "none";
+// });
+
+alert("This Website is Under Development");
