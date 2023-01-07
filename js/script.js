6e46d590c001b907f07699ef0c3b4817bc4ba916
rizkyap90s <rizkie.kudo@gmail.com>
2023-01-07 22:10:41 +0700

    add new

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// const indev = document.querySelector(".indev");
-// window.addEventListener("click", function () {
-//   indev.style.display = "none";
-// });
-
-alert("This Website is Under Development");
